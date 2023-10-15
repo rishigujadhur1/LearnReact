e44b2abfe6b8720a7f0e32351d45206c7c10e997
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-10-15 19:26:06 +0400

    interview questions

---
+++ activities/interview/questions/answers.js
@@ -0,0 +1,29 @@
+// // Answer 1:
+// // React is a JavaScript library for building user interfaces. Some key features of React that make it popular include its component-based architecture, virtual DOM for efficient updates, one-way data flow, and a strong and active community.
+
+// Answer 2:
+// JSX (JavaScript XML) is a syntax extension for JavaScript that allows you to write HTML-like code within your JavaScript code. React components use JSX to describe the structure of the UI elements they render. Babel transpiles JSX into JavaScript that can be understood by the browser.
+
+// Answer 3:
+// React components are the building blocks of a React application. Functional components are JavaScript functions that return JSX and are used for simple, stateless UI elements. Class components are JavaScript classes that extend React.Component and are used for more complex components that require state management and lifecycle methods.
+
+// Answer 4:
+// The virtual DOM is an in-memory representation of the actual DOM. React uses the virtual DOM to optimize updates by comparing it to the real DOM and making minimal changes. This reduces the number of direct manipulations to the actual DOM, improving performance.
+
+// Answer 5:
+// State management in React involves storing and managing component-specific data. The setState method is used to update the state of a component. When setState is called, React re-renders the component and its child components, reflecting the updated state in the UI.
+
+// Answer 6:
+// Props (short for properties) are used to pass data from parent components to child components. Props allow parent components to configure child components and make them reusable. Data is passed as attributes to the child component in JSX.
+
+// Answer 7:
+// React Router is a library that enables client-side routing in a single-page React application. It allows you to define routes, and when the URL changes, React Router renders the corresponding component. This provides a seamless, single-page application experience.
+
+// Answer 8:
+// Lifecycle methods are special methods in class components that are invoked at different stages of a component's lifecycle. Common lifecycle methods include componentDidMount, componentDidUpdate, and componentWillUnmount. They are used to perform tasks like data fetching, updates, and cleanup.
+
+// Answer 9:
+// React hooks are functions that allow functional components to use state and lifecycle features previously available only in class components. Hooks like useState and useEffect enable functional components to manage state and perform side effects, making them more powerful and reusable.
+
+// Answer 10:
+// Redux is a state management library for React that helps manage global state. It provides a single source of truth for application data and allows components to access and update that data. Redux is particularly useful for complex, interconnected state management scenarios.
---
+++ activities/interview/questions/questions.js
@@ -0,0 +1,29 @@
+// Question 1:
+// What is React, and what are some key features that make it a popular choice for building user interfaces?
+
+// Question 2:
+// What is JSX in React, and how does it work?
+
+// Question 3:
+// Explain the concept of React components. What are functional components and class components, and when would you use each?
+
+// Question 4:
+// What is the virtual DOM in React, and how does it optimize the rendering process?
+
+// Question 5:
+// How does state management work in React, and what is the significance of the "setState" method?
+
+// Question 6:
+// What is the purpose of React props, and how do you pass data from parent components to child components using props?
+
+// Question 7:
+// What is the role of React Router in a React application, and how does it facilitate client-side routing?
+
+// Question 8:
+// Explain the concept of lifecycle methods in React. What are some common lifecycle methods, and when are they invoked?
+
+// Question 9:
+// What are React hooks, and how do they change the way you work with state and side effects in functional components?
+
+// Question 10:
+// What is the role of Redux in a React application, and how does it help manage global state?
